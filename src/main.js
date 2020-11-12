990b112a7eaf881c5c9e2312d282b25fcdf4ad50
17866636590 <455116351@qq.com>
2020-11-12 14:20:22 +0800

    '首页抽离'

--- src/main.js
+++ src/main.js
@@ -6,6 +6,24 @@ import store from './store'
 import './assets/styles/base.scss'
 Vue.config.productionTip = false
 
+// 数字变成变单位的数字
+/*
+Vue.filter()
+第一个参数  是过滤器的名字
+第二个参数  是回调函数,必须有返回值
+*/
+// 注册全局过滤器
+Vue.filter('formatYi', (value) => {
+  var Yi = Math.pow(10, 8) // 10,0000,000
+  /*eslint-disable */
+  if (value > Yi) {
+    return `${(value/Yi).toFixed(2)}亿`
+  } else {
+    return `${(value/Math.pow(10,4)).toFixed(2)}万`
+  }
+  /* eslint-enable */
+})
+
 new Vue({
   router,
   store,
--- src/views/Home/index.vue
+++ src/views/Home/index.vue
@@ -1,32 +1,34 @@
 <template>
    <div class="page-home">
-    <header class="index-header">
-      <a href="mine.html?cpid=0">
-        <div class="header-user">
-          <div class="user-btn"></div>
-        </div>
-      </a>
-      <div class="header-logo"></div>
-      <a href="search.html?cpid=0">
-        <div class="header-search"></div>
-      </a>
-    </header>
-    <!-- <Swiper :autoplay="2000" :loop="true" @change="changeHandler">
-      <Swiper-item>1</Swiper-item>
-      <Swiper-item>2</Swiper-item>
-      <Swiper-item>3</Swiper-item>
-    </Swiper> -->
-    <Swiper ref='xxx'>
-      <Swiper-item>1</Swiper-item>
-      <Swiper-item>2</Swiper-item>
-      <Swiper-item>3</Swiper-item>
+    <index-header></index-header>
+    <!-- 下载数据是异步，而实例化是直接在组件中，不在一个文件里，没有关联 ，v-if="bannerList.length>0"，有数据的时候加载他-->
+    <div class="index-main">
+      <Swiper class="my-swiper" v-if="bannerList.length>0" :autoplay='2000'>
+      <Swiper-item v-for="item in bannerList" :key="item.id">
+        <img :src="item.imageurl" alt />
+      </Swiper-item>
     </Swiper>
-    <!-- <Swiper >
-      <Swiper-item>1</Swiper-item>
-      <Swiper-item>2</Swiper-item>
-      <Swiper-item>3</Swiper-item>
-    </Swiper> -->
-   </div>
+    <!-- 导航 -->
+     <index-nav></index-nav>
+     <!-- 生成漫画数据 -->
+     <index-recommend v-for="item in  recommentList" :key="item.specialid" :info="item"></index-recommend>
+      <div class="my-icp">
+        <a
+          class="record"
+          href="http://www.beian.gov.cn/portal/registerSystemInfo?recordcode=31011202006214"
+          target="_blank"
+        >
+          <img class="img" src="https://wechatapp.zhuishushenqi.com/mhd/201711/gongan.jpg" />
+          <div>沪公网安备 31011202006214号</div>
+        </a>
+        <div class="licence">
+          增值电信业务经营许可证沪B2-20170022
+          <br />网络文化经营许可证沪网文（2016）3206-227号
+          <br />出版物经营许可证新出发沪批字第U7659号
+        </div>
+      </div>
+    </div>
+    </div>
 </template>
 
 <script>
@@ -37,94 +39,171 @@
 // 写上index时,就不用写那么复杂的路径了,因为直接加载该目录下的头文件
 import { Swiper, SwiperItem } from '@/components/Swiper'
 
-import { getBanner } from '@/api/cartoon'
+import { getBanner, getIndexRecomment } from '@/api/cartoon'
+
+// 引入抽离的组件
+import indexHeader from './components/indexHeader'
+import indexNav from './components/indexNav'
+import indexRecommend from './components/indexRecommend'
 export default {
   name: 'Home',
+  data () {
+    /*
+    数据能存放的地方
+    考虑的地方
+    data,在当前组件里用
+    props,外部传入数据
+    computed,依赖前面两个计算出来的
+    state,跨组件使用
+    getter,通过依赖state计算出来的
+    */
+    return {
+      bannerList: [],
+      recommentList: []
+    }
+  },
   // 自定义组件要先调用一下才可以使用
   components: {
     Swiper,
-    SwiperItem
+    SwiperItem,
+    indexHeader,
+    indexNav,
+    indexRecommend
   },
   methods: {
     changeHandler (payload) {
       console.log('我自己index:', payload)
+    },
+    getBanner () {
+      getBanner().then(res => {
+      // console.log(res)
+      /*
+      漫画岛的每一个接口 都有code的字段
+      如果字段是200的话,这个接口OK
+      */
+        if (res.code === 200) {
+          this.bannerList = res.info
+        } else {
+        // 不OK,就报错
+        // 在这里先alert进行报错,后续 我们可以用van 组件库的组件
+          alert(res.code_msg)
+        }
+      }).catch((err) => {
+        alert('网络异常,请稍后', err)
+      })
+    },
+    getIndexRecomment () {
+      getIndexRecomment().then(res => {
+        // console.log(res)
+        if (res.code === 200) {
+          this.recommentList = res.info
+          console.log(this.recommentList)
+        } else {
+        // 不OK,就报错
+        // 在这里先alert进行报错,后续 我们可以用van 组件库的组件
+          alert(res.code_msg)
+        }
+      }).catch((err) => {
+        alert('网络异常,请稍后', err)
+      })
     }
   },
   mounted () {
     // console.log(this.$refs.xxx)
     // console.log(this.$refs.xxx.$el)
   },
-  // created () {
-  //   fetch('https://mhd.zhuishushenqi.com/comic_v2/getproad?apptype=8&appversion=1.0&channel=web-app&adgroupid=123')
-  //     .then(res => res.json())
-  //     .then(res => {
-  //       console.log(res)
-  //     })
-  // }
-
-  // 处理不能跨域的网站https://api.asilu.com/today/
-  // created () {
-  //   fetch('http://localhost:8080/migu/today/ ')
-  //     .then(res => res.json())
-  //     .then(res => {
-  //       console.log(res)
-  //     })
-  // }
-
   created () {
-    getBanner().then(res => {
-      console.log(res)
-    })
+    // 下载轮播图的数据
+    this.getBanner()
+    // 获取首页推荐漫画的数据
+    this.getIndexRecomment()
   }
 }
 </script>
 
 <style lang='scss' scoped>
 @import "~@/assets/styles/mixin.scss";
+.index-main{
+  flex: 1;
+  overflow-y: auto;
+}
     .page-home{
+      .my-swiper{
+        img{
+          width: 100%;
+        }
+      }
         display: flex;
         flex-direction:column ;
         height: 100%;
-        .index-header{
-          position: relative;
-          /* border-bottom: 1px solid #ededed;
-           &::after{
-             content: "";
-             position: absolute;
-             width: 100%;
-             height: 1px;
-             left: 0;
-             bottom: 0;
-             background: #ededed;
-            transform: scaleY(0.5);
-           }*/
-           @include border-bottom;
-            display: flex;
-            height: 44px;
-            //三者等分平铺
-            justify-content: space-between;
-            align-items: center;
-            padding: 0 20px;
-            box-sizing: border-box;
-            .user-btn{
-                width: 25px;
-                height: 25px;
-                background: url(~@/assets/icon/user-btn.png);
-                background-size: 100%;
-            }
-            .header-logo{
-                width: 92px;
-                height: 28px;
-                background: url(~@/assets/logo.png) no-repeat;
-                background-size: 100%;
-            }
-            .header-search{
-                 width: 25px;
-                height: 25px;
-                background: url(~@/assets/icon/header-search.png);
-                background-size: 100%;
-            }
-        }
+}
 
+</style>
+
+<style lang="scss">
+.swiper-container {
+  width: 100%;
+  height: 180px;
+  img {
+    width: 100%;
+  }
+  .swiper-pagination-bullet {
+    opacity: 1;
+    vertical-align: middle;
+    width: 6px;
+    height: 6px;
+    border-radius: 50%;
+    background-color: hsla(0, 0%, 100%, 0.7);
+  }
+
+  .swiper-pagination-bullet-active {
+    width: 20px;
+    height: 10px;
+    background: url("../../assets/icon/dot.png") no-repeat;
+    background-size: 100%;
+  }
+}
+
+.my-icp {
+  padding: 5px 0;
+  font-size: 12px;
+  .record {
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    .img {
+      width: 15px;
+      height: 15px;
+      margin-right: 4px;
     }
+  }
+  .licence {
+    margin-top: 4px;
+    text-align: center;
+  }
+}
+
+.go-top {
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+  width: 45px;
+  height: 45px;
+  background-color: rgba(0, 0, 0, 0.5);
+  border-radius: 50%;
+  text-align: center;
+  position: fixed;
+  z-index: 999;
+  bottom: 50px;
+  right: 5px;
+  color: #fff;
+  .icon-arrow-up {
+    width: 16px;
+    height: 10px;
+    margin-bottom: 4px;
+    background: url("../../assets/icon/icon-arrow-up.png") no-repeat;
+    background-size: 100%;
+  }
+}
 </style>
